6f7bc2de344ad5e808f60081ad36fbfdddf9030d
George Spanos <gespa11019@gmail.com>
2020-04-18 20:23:24 +0300

    Design .11

--- apps/job-finder/src/app/core/core.module.ts
+++ apps/job-finder/src/app/core/core.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
-import { MenuComponent } from './menu/menu.component';
+import { LadingCardsComponent } from './landing-cards/landing-cards.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { TitleComponent } from './title/title.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
     declarations: [
-        MenuComponent,
-        TitleComponent
+        LadingCardsComponent,
+        TitleComponent,
+        MenuComponent
     ],
 
     imports: [
@@ -18,11 +21,13 @@ import { TitleComponent } from './title/title.component';
         MatToolbarModule,
         MatButtonModule,
         FlexLayoutModule,
-        MatCardModule
+        MatCardModule,
+        MatListModule
     ],
     exports: [
-        MenuComponent,
-        TitleComponent
+        LadingCardsComponent,
+        TitleComponent,
+        MenuComponent
     ]
 })
 export class CoreModule {
---
+++ apps/job-finder/src/app/core/landing-cards/landing-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'job-finder-landing-cards',
+  templateUrl: './landing-cards.component.html',
+  styleUrls: ['./landing-cards.component.scss']
+})
+export class LadingCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
