e56ace9d72d70e580254468829a6e95f9c62016d
George Spanos <gespa11019@gmail.com>
2020-05-01 17:52:32 +0300

    Route Fade on Forms

--- apps/job-finder/src/app/voc-test/voc-test.component.ts
+++ apps/job-finder/src/app/voc-test/voc-test.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsService } from './forms.service';
+import { fadeInAnimation } from '../animations';
 
 @Component({
   selector: 'job-finder-voc-test',
   templateUrl: './voc-test.component.html',
-  styleUrls: ['./voc-test.component.scss']
+  styleUrls: ['./voc-test.component.scss'],
+  animations: [fadeInAnimation],
+  host: { '[@fadeInAnimation]': '' }
 })
 export class VocTestComponent implements OnInit {
 
